f58022a3cd07d9bc6ad9552a916148cacac75fe0
OrthofeetDerek <dereknashdev@gmail.com>
2022-07-26 11:00:37 -0400

    auth v5

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import '@aws-amplify/ui-react/styles.css';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 
 function App() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Hello from V2</h1>
+          <h1>We now have Auth!</h1>
         </header>
+        {/* <Button onClick={signOut}>Sign Out</Button> */}
       </div>
     );
   }
 
-export default App;
+export default withAuthenticator(App);
