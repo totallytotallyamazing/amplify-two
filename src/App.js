4221e764ffd49e249f1730883eecc2342ae426ba
OrthofeetDerek <dereknashdev@gmail.com>
2022-07-26 10:39:21 -0400

    auth v4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from "./logo.svg";
-import "@aws-amplify/ui-react/styles.css";
-import {
-  withAuthenticator,
-  Button,
-  Heading,
-  Image,
-  View,
-  Card,
-} from "@aws-amplify/ui-react";
+import logo from './logo.svg';
+import './App.css';
 
-function App({ signOut }) {
-  return (
-    <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
-  );
-}
+function App() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Hello from V2</h1>
+        </header>
+      </div>
+    );
+  }
 
-export default withAuthenticator(App);
+export default App;
